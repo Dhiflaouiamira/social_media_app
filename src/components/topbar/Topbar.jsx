5416322b93d50defc4783a88de2f47b93ec13a1c
dhiflaoui <dhiflaouiamira98@gmail.com>
2023-08-25 15:27:42 +0200

    add components topbar and home page

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./topbar.css"
+
+
+export default function Topbar() {
+  return (
+    <div className='Topbarcontainer'>Topbar
+           <div className='TopbarCenter'>
+
+
+            <span className='logo'> TrendWave </span>
+           </div>
+           <div className='TopbarLeft'></div>
+           <div className='TopbarRight'></div>
+
+    </div>
+  )
+}
